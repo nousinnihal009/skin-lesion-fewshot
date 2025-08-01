627e54273ed191c897d8dff9cbcdac7048303a2a
nousinnihal009 <nousinnihal2004@gmail.com>
2025-08-02 00:31:56 +0530

    Update visualizer.py

--- utils/visualizer.py
+++ utils/visualizer.py
@@ -1,27 +1,32 @@
+import os
+import torch
 import matplotlib.pyplot as plt
-import seaborn as sns
+from sklearn.manifold import TSNE
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
-def plot_accuracy_curve(train_acc, val_acc, save_path=None):
-    plt.figure(figsize=(8, 5))
-    plt.plot(train_acc, label='Train Accuracy')
-    plt.plot(val_acc, label='Validation Accuracy')
-    plt.xlabel('Epochs')
-    plt.ylabel('Accuracy')
-    plt.title('Accuracy Curve')
-    plt.legend()
-    plt.grid(True)
+
+def plot_tsne(embeddings, labels, save_path=None):
+    """
+    Plot t-SNE of embeddings.
+    """
+    tsne = TSNE(n_components=2, perplexity=30, n_iter=1000, random_state=42)
+    reduced = tsne.fit_transform(embeddings.cpu().numpy())
+    plt.figure(figsize=(8, 6))
+    scatter = plt.scatter(reduced[:, 0], reduced[:, 1], c=labels.cpu().numpy(), cmap='tab10', s=12)
+    plt.colorbar(scatter, ticks=range(len(torch.unique(labels))))
+    plt.title("t-SNE of Embeddings")
     if save_path:
         plt.savefig(save_path)
     plt.close()
 
-def plot_confusion_matrix(y_true, y_pred, class_names, save_path=None):
-    cm = confusion_matrix(y_true, y_pred, labels=class_names)
-    plt.figure(figsize=(10, 8))
-    sns.heatmap(cm, annot=True, fmt='d', xticklabels=class_names, yticklabels=class_names, cmap='Blues')
-    plt.xlabel('Predicted')
-    plt.ylabel('True')
-    plt.title('Confusion Matrix')
+
+def plot_confusion_matrix(preds, targets, class_names, save_path=None):
+    """
+    Plot and save confusion matrix.
+    """
+    cm = confusion_matrix(targets.cpu(), preds.cpu(), labels=range(len(class_names)))
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=class_names)
+    disp.plot(cmap=plt.cm.Blues, xticks_rotation=45)
     if save_path:
         plt.savefig(save_path)
     plt.close()
